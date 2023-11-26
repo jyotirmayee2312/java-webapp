2aaaa13e55b47613ed2b314862acf2ce41245bc7
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-11-26 23:52:22 +0530

    Update unzip_and_run.sh

--- unzip_and_run.sh
+++ unzip_and_run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Define the target file
+TARGET_FILE="/target/spring-boot-web.jar.original"
+
+# Check if the file exists and remove it
+if [ -f "$TARGET_FILE" ]; then
+    echo "Removing existing file: $TARGET_FILE"
+    rm "$TARGET_FILE" || exit 1
+fi
+
 # Download artifact from S3
 aws s3 cp s3://codepipeline-ap-south-1-803549830893/JYOTI-01 /opt/myapp/artifact.zip || exit 1
 
