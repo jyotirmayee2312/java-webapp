d08a463c921767e14c991ed7a2173f652c3bc574
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-11-27 08:00:28 +0530

    Update download_artifact.sh

--- download_artifact.sh
+++ download_artifact.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Remove the existing file if it exists
+TARGET_FILE="/opt/myapp/extracted/target/spring-boot-web.jar.original"
+
+if [ -f "$TARGET_FILE" ]; then
+    echo "Removing existing file: $TARGET_FILE"
+    rm "$TARGET_FILE" || exit 1
+fi
+
 # Download artifact from S3
 aws s3 cp s3://codepipeline-ap-south-1-803549830893/JYOTI-01.zip /opt/myapp/artifact.zip || exit 1
 
