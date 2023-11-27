5b2f345068f2842de0be95e1b5317910135641ed
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-11-27 08:02:39 +0530

    Update start_application.sh

--- start_application.sh
+++ start_application.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Define the target file
+TARGET_FILE="/opt/myapp/extracted/target/spring-boot-web.jar.original"
+
+# Check if the file exists and remove it
+if [ -f "$TARGET_FILE" ]; then
+    echo "Removing existing file: $TARGET_FILE"
+    rm "$TARGET_FILE" || exit 1
+fi
+
 # Run the Java application using the .war file
 java -jar /opt/myapp/extracted/*.war
