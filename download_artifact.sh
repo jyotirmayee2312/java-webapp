974515342146dbf590c737715312374880669861
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-11-27 07:52:44 +0530

    Update download_artifact.sh

--- download_artifact.sh
+++ download_artifact.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download artifact from S3
-aws s3 cp s3://your-bucket-name/path/to/artifact.zip /opt/myapp/artifact.zip || exit 1
+aws s3 cp s3://codepipeline-ap-south-1-803549830893/JYOTI-01.zip /opt/myapp/artifact.zip || exit 1
 
 # Unzip the artifact
 unzip -q /opt/myapp/artifact.zip -d /opt/myapp/extracted || exit 1
