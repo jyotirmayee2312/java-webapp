5317f9cd13208484849b4fae3ac62c9de471f4e7
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-11-27 07:30:44 +0530

    Update unzip_and_run.sh

--- unzip_and_run.sh
+++ unzip_and_run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Copy the new .war file to the deployment directory
-cp /opt/myapp/myapp.zip /opt/myapp/myapp.war || exit 1
-
-# Start the Java application using the .war file
-java -jar /opt/myapp/myapp.war
+# Run the Java application using the .war file
+java -jar /opt/myapp/extracted/*.war
