b28986d22f88758a615ff513a1683b092c4df066
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-11-27 07:22:48 +0530

    Update remove_existing_file.sh

--- remove_existing_file.sh
+++ remove_existing_file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Remove the existing file if it exists
-TARGET_FILE="/opt/myapp/spring-boot-web.jar.original"
+# Remove existing .war file if it exists
+TARGET_FILE="/opt/myapp/myapp.war"
 
 if [ -f "$TARGET_FILE" ]; then
-    echo "Removing existing file: $TARGET_FILE"
+    echo "Removing existing .war file: $TARGET_FILE"
     rm -f "$TARGET_FILE" || exit 1
 fi
