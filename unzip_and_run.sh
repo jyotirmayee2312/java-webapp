054474fc111d0f047eee2d27f63ddb1849ad8972
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-11-27 07:23:49 +0530

    Update unzip_and_run.sh

--- unzip_and_run.sh
+++ unzip_and_run.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# Define the target file
-TARGET_FILE="/target/spring-boot-web.jar.original"
+# Copy the new .war file to the deployment directory
+cp /opt/myapp/myapp.zip /opt/myapp/myapp.war || exit 1
 
-# Check if the file exists and remove it
-if [ -f "$TARGET_FILE" ]; then
-    echo "Removing existing file: $TARGET_FILE"
-    rm "$TARGET_FILE" || exit 1
-fi
-
-# Download artifact from S3
-aws s3 cp s3://codepipeline-ap-south-1-803549830893/JYOTI-01 /opt/myapp/artifact.zip || exit 1
-
-# Unzip the artifact
-unzip -q /opt/myapp/artifact.zip -d /opt/myapp/extracted || exit 1
-
-# Rename the new JAR file
-mv /opt/myapp/extracted/spring-boot-web.jar /opt/myapp/extracted/app.jar || exit 1
-
-# Run the Java JAR file
-java -jar /opt/myapp/extracted/app.jar
+# Start the Java application using the .war file
+java -jar /opt/myapp/myapp.war
